5f281068f75657b26a42e60466dfb3c90bd6df83
lena <lenawanel@proton.me>
2023-07-16 08:39:07 +0200

    make test_runner print err mesage

--- src/bin/test_runner.rs
+++ src/bin/test_runner.rs
@@ -3,10 +3,11 @@ use amd64_emu::emu;
 pub fn main() {
     let mut emu = emu::Emu::new(1024 * 1024 * 16);
     emu.load("./tests/a.out");
-    let _ = emu.run_emu();
+    let res = emu.run_emu();
     #[cfg(debug_assertions)]
     {
         emu.trace();
         emu.print_stack::<u64, 8>(emu.stack_depth);
     }
+    res.unwrap();
 }
